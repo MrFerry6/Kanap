a8af3146f73bddd530af1839177581ff38d741df
Ferry <you@example.com>
2022-02-15 14:04:07 +0000

    Add existing file

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,74 +1,67 @@
-
-fetch(createProductUrl())
-.then(response => {
-    let data = response.json();
-    data.then(post =>{ 
-        storeProduct(createProductArray());
-        //createProductsCart();
-    })  
-})
+//localStorage.clear()
+storeItem(createProductObject());
 
 
-function createProductsCart() {
+function storeItem(product){
+    if(product !==null){
+        let id = product.id + '_' + product.color;
+        addToCart(id, product);
+    }
+}
+
+function addToCart(id, product) {
+    if (checkInCart(id) === true) {
+        addQuantitytoExisting(id, product);
+    } else {
+        localStorage.setItem(id, JSON.stringify(product));
+    };
+}
+
+function addQuantitytoExisting(id, product) {
+    let storedProduct = JSON.parse(localStorage.getItem(id));
+    storedProduct.quantity =
+        (parseInt(storedProduct.quantity) +
+            parseInt(product.quantity)).toString();
+    localStorage.removeItem(id);
+    localStorage.setItem(id, JSON.stringify(storedProduct));
+}
+
+function checkInCart(id) {
     localStorageProducts = JSON.parse(JSON.stringify(localStorage));
-
+    let isInCart = false
     for (var [key, value] of Object.entries(localStorageProducts)) {
-        let product = JSON.parse(value);
-        let cartIems = document.getElementById('cart__items');
-        let article = createArticle(product);
-        let imageContainer = createImageContainer();
-        let img = createImg(product);
-        imageContainer.append(img);
-        article.append(imageContainer);
-        cartIems.append(article);
+        if (checkStorageId(key, id)){
+            isInCart = true;
+        }
     }
+    return isInCart;
+}
 
-function createArticle(product) {
-    let article = document.createElement('article');
-    article.class = 'cart__item';
-    article.id = product._id;
-    article.color = product.color;
-    return article;
+function checkStorageId(key, id) {
+    if (key === id) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-function createImageContainer() {
-    let imageContainer = document.createElement('div');
-    imageContainer.class = 'cart__item__img';
-    return imageContainer;
-}
-function createImg(product) {
-    console.log(product);
-    let img = document.createElement('img');
-    img.alt = product.altText;
-    console.log(product.altText);
-    img.src = product.imageUrl;
-    return img;
-}
-
-function storeProduct(product){
-    localStorage.setItem(product._id, JSON.stringify(product));
-}
-
-function createProductArray()
+function createProductObject()
 {
-    let product;
-    product.id = getUrlParameter('id');
-    product.color = getUrlParameter('color');
-    product.quantity= getUrlParameter('quantity');
-    return product;
+    if(getUrlParameter('id') !== null){
+        let product = {
+            id : getUrlParameter('id'),
+            color : getUrlParameter('color'),
+            quantity :  getUrlParameter('quantity')
+        };
+        return product;
+    }else{
+        return null;
+    }
 }
 
-function getProducts()
-{
-}
 function getUrlParameter(key){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const paramenter = urlParams.get(key);
     return paramenter;
-}
-function createProductUrl(){
-    const  product = 'http://localhost:3000/api/products/' + getUrlParameter('id');
-    return product;
 }
